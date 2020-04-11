50dd052b5c4587901cff059df9ecdf1268f9d1c9
Arik Kfir <arik@kfirs.com>
2020-04-11 22:39:46 +0300

    Require prometheus registry for metrics middleware
    This is better than using the default registry, as it does not prevent clients to properly cleanup.

--- chi_metrics_middleware.go
+++ chi_metrics_middleware.go
@@ -9,36 +9,38 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-func Metrics(next http.Handler) http.Handler {
-	requestsCounter := prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name:        "requests",
-			Help:        "How many HTTP requests processed, partitioned by status code, method and HTTP path.",
+func Metrics(registry *prometheus.Registry) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		requestsCounter := prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Name:        "requests",
+				Help:        "How many HTTP requests processed, partitioned by status code, method and HTTP path.",
+				ConstLabels: prometheus.Labels{"service": "gate"},
+			},
+			[]string{"code", "method", "path"},
+		)
+		registry.MustRegister(requestsCounter)
+
+		latencyHistogram := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Name:        "latency",
+			Help:        "How long it took to process the request, partitioned by status code, method and HTTP path.",
 			ConstLabels: prometheus.Labels{"service": "gate"},
+			Buckets:     []float64{300, 1200, 5000},
 		},
-		[]string{"code", "method", "path"},
-	)
-	prometheus.MustRegister(requestsCounter)
+			[]string{"code", "method", "path"},
+		)
+		registry.MustRegister(latencyHistogram)
 
-	latencyHistogram := prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:        "latency",
-		Help:        "How long it took to process the request, partitioned by status code, method and HTTP path.",
-		ConstLabels: prometheus.Labels{"service": "gate"},
-		Buckets:     []float64{300, 1200, 5000},
-	},
-		[]string{"code", "method", "path"},
-	)
-	prometheus.MustRegister(latencyHistogram)
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
-		defer func() {
-			statusCodeStr := strconv.Itoa(ww.Status())
-			duration := float64(time.Since(start).Nanoseconds()) / 1000000
-			requestsCounter.WithLabelValues(statusCodeStr, r.Method, r.URL.Path).Inc()
-			latencyHistogram.WithLabelValues(statusCodeStr, r.Method, r.URL.Path).Observe(duration)
-		}()
-		next.ServeHTTP(ww, r)
-	})
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+			defer func() {
+				statusCodeStr := strconv.Itoa(ww.Status())
+				duration := float64(time.Since(start).Nanoseconds()) / 1000000
+				requestsCounter.WithLabelValues(statusCodeStr, r.Method, r.URL.Path).Inc()
+				latencyHistogram.WithLabelValues(statusCodeStr, r.Method, r.URL.Path).Observe(duration)
+			}()
+			next.ServeHTTP(ww, r)
+		})
+	}
 }
